07c7f9062cb3e86b53ecb1ee9d7f06c7d6545183
MG <magallardo11@outlook.com>
2022-09-27 13:28:43 -0700

    food array complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,59 +4,62 @@ const foodArray = [
     {
         food:"Pizza",
         img:"assets/foodImages/pizza.avif",
-        alt:"Picture of Pizza",
-    }
+        alt:"Picture of a Pizza",
+    },
     {
         food:"Burger",
         img:"assets/foodImages/burger.avif",
-        alt:"Picture of Pizza",
-    }
+        alt:"Picture of a Burger",
+    },
     {
         food:"Pasta",
         img:"assets/foodImages/pasta.avif",
-        alt:"Picture of Pizza",
-    }
+        alt:"Picture of Pasta",
+    },
     {
         food:"Ribs",
         img:"assets/foodImages/ribs.avif",
-        alt:"Picture of Pizza",
-    }
+        alt:"Picture of Ribs",
+    },
     {
         food:"Tacos",
         img:"assets/foodImages/tacos.avif",
-        alt:"Picture of Pizza",
-    }
+        alt:"Picture of Tacos",
+    },
     {
         food:"Steak",
         img:"assets/foodImages/steak.avif",
-        alt:"Picture of Pizza",
-    }
+        alt:"Picture of a Steak",
+    },
     {
         food:"Wings",
         img:"assets/foodImages/wings.avif",
-        alt:"Picture of Pizza",
-    }
+        alt:"Picture of Wings",
+    },
     {
         food:"Sushi",
         img:"assets/foodImages/sushi.avif",
         alt:"Picture of Sushi",
-    }
+    },
     {
         food:"Salad",
         img:"assets/foodImages/salad.avif",
-        alt:"Picture of Salad",
-    }
+        alt:"Picture of a Salad",
+    },
     {
         food:"Fried Rice",
         img:"assets/foodImages/fried-rice.avif",
         alt:"Picture of Fried Rice",
-    }
+    },
 ];
 
-function generateFoodArray(data) {
+// function generateFoodArray(data) {
+// pizzaEl.children[0].textContent = data.food;
+// pizzaEl.children[1].children[0].setAttribute("src", data.img);
+// pizzaEl.children[1].children[0].setAttribute("alt", data.alt);
+// pizzaEl.children[1].children[0].setAttribute("style", "padding:10px;");
+// }
 
-}
-
-for (let i = 0; i < foodArray.length; i++) {
-    generateFoodArray(foodArray[i])
-}
+// for (let i = 0; i < foodArray.length; i++) {
+//     generateFoodArray(foodArray[i])
+// }
