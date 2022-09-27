1821ac066b93244a6b95475415c000a6e969e21c
MG <magallardo11@outlook.com>
2022-09-27 12:05:48 -0700

    Food Array changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,62 @@
 console.log ("this is a test")
+
+const foodArray = [
+    {
+        food:"Pizza",
+        img:"assets/foodImages/pizza.avif",
+        alt:"Picture of Pizza",
+    }
+    {
+        food:"Burger",
+        img:"assets/foodImages/burger.avif",
+        alt:"Picture of Pizza",
+    }
+    {
+        food:"Pasta",
+        img:"assets/foodImages/pasta.avif",
+        alt:"Picture of Pizza",
+    }
+    {
+        food:"Ribs",
+        img:"assets/foodImages/ribs.avif",
+        alt:"Picture of Pizza",
+    }
+    {
+        food:"Tacos",
+        img:"assets/foodImages/tacos.avif",
+        alt:"Picture of Pizza",
+    }
+    {
+        food:"Steak",
+        img:"assets/foodImages/steak.avif",
+        alt:"Picture of Pizza",
+    }
+    {
+        food:"Wings",
+        img:"assets/foodImages/wings.avif",
+        alt:"Picture of Pizza",
+    }
+    {
+        food:"Sushi",
+        img:"assets/foodImages/sushi.avif",
+        alt:"Picture of Sushi",
+    }
+    {
+        food:"Salad",
+        img:"assets/foodImages/salad.avif",
+        alt:"Picture of Salad",
+    }
+    {
+        food:"Fried Rice",
+        img:"assets/foodImages/fried-rice.avif",
+        alt:"Picture of Fried Rice",
+    }
+];
+
+function generateFoodArray(data) {
+
+}
+
+for (let i = 0; i < foodArray.length; i++) {
+    generateFoodArray(foodArray[i])
+}
